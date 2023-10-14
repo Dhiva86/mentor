808e8a14a9a674587172e63795cfd43a0068273f
Dhiva86 <dhivakar4510@gmail.com>
2023-10-14 10:13:28 +0530

    first commit

---
+++ student.js
@@ -0,0 +1,21 @@
+let students = [{
+        id: 1,
+        name: 'Dhiva',
+        subject: 'Node',
+        mentor: "Mentor1"
+    },
+    {
+        id: 2,
+        name: 'Ajith',
+        subject: 'JavaScript',
+        mentor: "Mentor2"
+    },
+    {
+        id: 3,
+        name: 'Sarath',
+        subject: 'React',
+        mentor: "Mentor3"
+    }
+]
+
+module.exports = students;
